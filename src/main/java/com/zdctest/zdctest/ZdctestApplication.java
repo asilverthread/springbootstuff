db54c16c8cc1f5701d651ceab6035fa157b74a94
asilverthread <zachary@cortesecrew.com>
2018-03-14 15:27:14 -0400

    Initial commit

---
+++ src/main/java/com/zdctest/zdctest/ZdctestApplication.java
@@ -0,0 +1,12 @@
+package com.zdctest.zdctest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZdctestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZdctestApplication.class, args);
+	}
+}
---
+++ src/test/java/com/zdctest/zdctest/ZdctestApplicationTests.java
@@ -0,0 +1,16 @@
+package com.zdctest.zdctest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ZdctestApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
